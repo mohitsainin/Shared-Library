6b25841a276ff65a9449f1c6f999c8e2c479794a
mohitsainin <it.mohitsaini@gmail.com>
2024-12-17 16:45:36 +0530

    Update bug_analysis.groovy

--- vars/bug_analysis.groovy
+++ vars/bug_analysis.groovy
@@ -1,1 +1,11 @@
-
+def call() {
+    withSonarQubeEnv('SonarQube') {
+        def scannerHome = tool name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+        sh """
+            ${scannerHome}/bin/sonar-scanner \
+            -Dsonar.projectKey=salary-api \
+            -Dsonar.sources=. \
+            -Dsonar.host.url=http://65.2.175.160:9000
+        """
+    }
+}
