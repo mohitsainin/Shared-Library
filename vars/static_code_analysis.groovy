711248f71914f8358e6dd71a63d0f7d8edf90aa4
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 16:50:03 +0530

    Update static_code_analysis.groovy

--- vars/static_code_analysis.groovy
+++ vars/static_code_analysis.groovy
@@ -3,10 +3,13 @@ def call(String sonarServerName) {
         def scannerHome = tool name: 'Sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
         sh """
             ${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=salary-api \
-            -Dsonar.sources=. \
+            -Dsonar.projectKey=salary \
+            -Dsonar.projectName=Salary-api \
             -Dsonar.host.url=http://100.25.17.63:9000 \
             -Dsonar.login=squ_4f371ade2db48dafec85157cda9d3d07e6a5c456 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.issue.types=CODE_SMELL \
+            -Dsonar.report.export.path=target/static-code-analysis-report.json
         """
     }
 }
