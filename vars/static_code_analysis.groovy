89c0660ddc6f4c202454c081c2042983748242b9
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 16:46:38 +0530

    Update static_code_analysis.groovy

--- vars/static_code_analysis.groovy
+++ vars/static_code_analysis.groovy
@@ -5,7 +5,8 @@ def call(String sonarServerName) {
             ${scannerHome}/bin/sonar-scanner \
             -Dsonar.projectKey=salary-api \
             -Dsonar.sources=. \
-            -Dsonar.host.url=http://65.2.175.160:9000
+            -Dsonar.host.url=http://100.25.17.63:9000 \
+            -Dsonar.login=squ_4f371ade2db48dafec85157cda9d3d07e6a5c456 \
         """
     }
 }
