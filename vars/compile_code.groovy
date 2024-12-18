cf5e21bc2d1417e27d9afe33703885efad6d3a7f
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 13:23:53 +0530

    Update compile_code.groovy

--- vars/compile_code.groovy
+++ vars/compile_code.groovy
@@ -7,7 +7,7 @@ def call(String mavenToolName) {
         sh """
             echo "Compilation Report" > target/compile-report.txt
             echo "Maven compile phase completed successfully." >> target/compile-report.txt
-            if [ -d target/classes ] && [ "$(ls -A target/classes)" ]; then
+            if [ -d target/classes ] && [ "\$(ls -A target/classes)" ]; then
                 echo "Compilation was successful. Classes are available." >> target/compile-report.txt
             else
                 echo "Compilation failed. No compiled classes found." >> target/compile-report.txt
