ea462d0bd7d0e48f8f534244bb621feb333de6ed
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 15:03:48 +0530

    Update dependency.groovy

--- vars/dependency.groovy
+++ vars/dependency.groovy
@@ -1,8 +1,17 @@
 def call(String mavenToolName) {
     script {
-     
+        // Validate the mavenToolName to avoid NullPointerException
+        if (mavenToolName == null || mavenToolName.trim().isEmpty()) {
+            error "Maven tool name is not configured or passed correctly!"
+        }
+        
+        // Get the Maven tool path
         def mvnHome = tool mavenToolName
-        sh "${mvnHome}/bin/mvn org.owasp:dependency-check-maven:check -Dformat=HTML"            
+        
+        // Run the OWASP Dependency Check
+        sh "${mvnHome}/bin/mvn org.owasp:dependency-check-maven:check -Dformat=HTML"
+        
+        // Archive the generated OWASP dependency check report
         archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: true
     }
 }
