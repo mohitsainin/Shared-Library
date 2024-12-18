7d89d16f4dae07bbd3db9d38cde08605b7e56e0d
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 15:47:43 +0530

    Update dependency.groovy

--- vars/dependency.groovy
+++ vars/dependency.groovy
@@ -1,10 +1,15 @@
-def call(String mavenToolName) {
+def call(String mavenToolName = 'Maven') {
     script {
        
-        def mvnHome = tool mavenToolName     
+        def mvnHome = tool mavenToolName
+        echo "Maven Home: ${mvnHome}"
+        sh "${mvnHome}/bin/mvn compile"
        
-        echo "Maven Home: ${mvnHome}"        
-        sh "${mvnHome}/bin/mvn org.owasp:dependency-check-maven:check -Dformat=HTML"
-         archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: false
+        sh "${mvnHome}/bin/mvn org.owasp:dependency-check-maven:check -Dformat=HTML -DoutputDirectory=target/dependency-check-report"
+   
+        sh 'ls -R target'
+      
+        archiveArtifacts artifacts: 'target/dependency-check-report/dependency-check-report.html', allowEmptyArchive: false
+        echo "Dependency check report archived successfully."
     }
 }
