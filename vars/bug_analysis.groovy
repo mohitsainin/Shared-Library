24b07a15801bbd6bb1903ca5151a772d975ddd55
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 17:00:28 +0530

    Update bug_analysis.groovy

--- vars/bug_analysis.groovy
+++ vars/bug_analysis.groovy
@@ -1,11 +1,16 @@
-def call() {
-    withSonarQubeEnv('SonarQube') {
-        def scannerHome = tool name: 'sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
+def call(String sonarServerName) {
+    withSonarQubeEnv(sonarServerName) {
+        def scannerHome = tool name: 'Sonar', type: 'hudson.plugins.sonar.SonarRunnerInstallation'
         sh """
             ${scannerHome}/bin/sonar-scanner \
-            -Dsonar.projectKey=salary-api \
-            -Dsonar.sources=. \
-            -Dsonar.host.url=http://65.2.175.160:9000
+            -Dsonar.projectKey=salary \
+            -Dsonar.projectName=Salary-api \
+            -Dsonar.host.url=http://100.25.17.63:9000 \
+            -Dsonar.login=squ_4f371ade2db48dafec85157cda9d3d07e6a5c456 \
+            -Dsonar.java.binaries=target/classes \
+            -Dsonar.issue.types=CODE_SMELL \
+            -Dsonar.report.export.path=target/static-code-analysis-report.json
         """
+      
     }
 }
