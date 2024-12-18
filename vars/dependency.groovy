10b66da59e0a20350b6d3e922eb60402eb0674f9
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 15:16:11 +0530

    Update dependency.groovy

--- vars/dependency.groovy
+++ vars/dependency.groovy
@@ -1,20 +1,12 @@
 def call(String mavenToolName) {
     script {
-        // Get the Maven tool path
-        def mvnHome = tool name: mavenToolName, type: 'Maven'
-        
-        // Check if the tool is found
-        if (mvnHome == null) {
-            error "Maven tool not found. Please ensure the tool is configured in Jenkins."
-        }
-
-        // Compile the project
+       
+        def mvnHome = tool mavenToolName
+             
         sh "${mvnHome}/bin/mvn compile"
-        
-        // Run the OWASP dependency check and generate the report
         sh "${mvnHome}/bin/mvn org.owasp:dependency-check-maven:check -Dformat=HTML"
         
-        // Archive the generated dependency check report
+        
         archiveArtifacts artifacts: 'target/dependency-check-report.html', allowEmptyArchive: true
     }
 }
