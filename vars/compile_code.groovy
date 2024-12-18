d613498a358169c6321e783033814a3e9ca806ee
mohitsainin <it.mohitsaini@gmail.com>
2024-12-18 11:01:38 +0530

    Update compile_code.groovy

--- vars/compile_code.groovy
+++ vars/compile_code.groovy
@@ -1,3 +1,7 @@
-def call() {
-       sh 'mvn compile'
+def call(String mavenToolName) {
+    script {
+       
+        def mvnHome = tool mavenToolName
+        sh "${mvnHome}/bin/mvn compile"
     }
+}
