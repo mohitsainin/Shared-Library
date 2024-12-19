1f90bc71214cd8627396e5535ebf3ca615c5b521
mohitsainin <it.mohitsaini@gmail.com>
2024-12-19 15:12:59 +0530

    Update clone_repo.groovy

--- vars/clone_repo.groovy
+++ vars/clone_repo.groovy
@@ -1,3 +1,13 @@
-def call() {
-        git branch: 'main', url: 'git@github.com:avengers-p11/salary-api.git'
+def call(String repositoryUrl, String branch = 'main') {
+    // Perform a clean checkout if the repository is already present
+    dir('workspace') {
+        if (fileExists('.git')) {
+            echo "Repository already cloned. Pulling the latest changes."
+            sh "git checkout ${branch}"
+            sh "git pull origin ${branch}"
+        } else {
+            echo "Cloning repository from ${repositoryUrl} and checking out branch ${branch}"
+            sh "git clone ${repositoryUrl} --branch ${branch} --single-branch ."
+        }
     }
+}
